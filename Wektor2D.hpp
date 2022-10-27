53375601db7a99b5a9feb35460c21ffdd52f95e8
chimek2107 <b.chimkowski@tuta.io>
2022-10-27 13:19:31 +0200

    jeden

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,36 @@
 class Wektor2D
 {
     // Tutaj napisz implementacje klasy Wektor2D
+public:
+	//ZMIENNE
+	double x;
+	double y;
+
+	//KONSTRUKTORY
+	Wektor2D() { x = 0; y = 0; }
+	Wektor2D(double a, double b) 
+	{ 
+		x = a; 
+		y = b; 
+	}
+
+
+	//SETTERY
+	void setX(double a) { x = a; }
+	void setY(double a) { y = a; }
+	//GETTERY
+	double getX() { return x; }
+	double getY() { return y; }
+
+
 };
+
+Wektor2D operator+ (Wektor2D d1, Wektor2D d2)
+{
+	return Wektor2D{ d1.x+d2.x, d1.y+d2.y };
+}
+
+double operator* (Wektor2D d1, Wektor2D d2)
+{
+	return d1.x*d2.x+d1.y*d2.y;
+}
